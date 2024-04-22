f48a9c1c1400024a6379ed5d5bf1ee141042735e
LVKinyanjui <leslievictorius@gmail.com>
2024-04-22 15:08:47 +0300

    Styled first react app with tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';     // Gives the app its general look
+                        // May conflict with tailwind
+
+function App() {
+  return (
+    <div className="text-center text-blue-500 font-light text-3xl">
+      <p>Hi Programmer</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  darkMode: false, //'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
